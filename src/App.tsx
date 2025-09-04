a81b69af045178e556f99241de1c10386b35e2f7
thanhlong2023 <caolong770709@gmail.com>
2025-09-04 08:13:29 +0700

    session 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import "./App.css";
-import AdminIndex from "./Session12/AdminIndex";
-import Calculation from "./Session12/Calculation";
-import ColorBox from "./Session12/ColorBox";
-import FormatName from "./Session12/FormatName";
-import ListCourse from "./Session12/ListCourse";
-import UserInfo from "./Session12/UserInfo";
+import Session12 from "./Session/Session12";
 
 function App() {
   return (
     <>
-      <ListCourse />
-      <Calculation />
-      <UserInfo />
-      <ColorBox />
-      <FormatName />
-      <AdminIndex />
+      <Session12 />
     </>
   );
 }
---
+++ src/Session/Session12.tsx
@@ -0,0 +1,20 @@
+import ListCourse from "../Session12/ListCourse";
+import Calculation from "../Session12/Calculation";
+import UserInfo from "../Session12/UserInfo";
+import ColorBox from "../Session12/ColorBox";
+import FormatName from "../Session12/FormatName";
+import AdminIndex from "../Session12/AdminIndex";
+
+export default function Session12() {
+  return (
+    <>
+      <h1>Session 12</h1>
+      <ListCourse />
+      <Calculation />
+      <UserInfo />
+      <ColorBox />
+      <FormatName />
+      <AdminIndex />
+    </>
+  );
+}
